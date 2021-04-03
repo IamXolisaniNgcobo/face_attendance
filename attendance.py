d7b17c953d2769f3fdae0d71a711c60f28e20528
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-04-03 16:30:50 +0200

    attendance webcam known

--- attendance.py
+++ attendance.py
@@ -40,8 +40,6 @@ def mark_attendance(name):
             f.writelines(f"\n{name},{date_string}")
 
 
-mark_attendance("Xolisani")
-
 encode_known_list = find_encodings(images)
 print("Encoding Complete")
 
@@ -56,8 +54,8 @@ while True:
 
     for encode_face, face_loc in zip(encode_current_frame, faces_current_frame):
 
-        matches = face_recognition.compare_faces(encode_known_list, encode_face)
-        face_dist = face_recognition.face_distance(encode_known_list, encode_face)
+        matches = face_recognition.compare_faces(encode_known_list[0], encode_face)
+        face_dist = face_recognition.face_distance(encode_known_list[0], encode_face)
 
         print(face_dist)
         match_index = np.argmin(face_dist)
@@ -70,6 +68,7 @@ while True:
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            mark_attendance(name)
 
     cv2.imshow('webcam', img)
     cv2.waitKey(1)
